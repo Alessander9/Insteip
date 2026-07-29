59fcb277196a383c72c10b4a54d22db76b3aa195
Alessander9 <Alessanderneedsponsors@gmail.com>
2026-07-29 06:41:48 -0500

    style: update acupuntura-china-7-meses card image to new assets/acupuntura_7meses.jpg

--- frontend/src/app/features/cursos/cursos.component.ts
+++ frontend/src/app/features/cursos/cursos.component.ts
@@ -200,7 +200,7 @@ export class PublicCursosComponent {
       description: 'Formación profesional intensiva para dominar los principios de la Medicina Tradicional China y aprender la práctica clínica de la acupuntura.',
       students: '+150',
       price: 'S/ 150/mes',
-      image: 'assets/acupuntura_7_meses_curso.jpg',
+      image: 'assets/acupuntura_7meses.jpg',
       customLink: '/cursos/acupuntura-china-7-meses'
     },
     {
